b8082e4052735c99d40a2b1287b6f5e93f72d122
vinhlt1999 <75895691+vinhlt1999@users.noreply.github.com>
2021-01-24 15:07:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Gaming Login Form Widget Tab Form,Login Forms,Sign up Forms,Registration Forms,News letter Forms,Elements"/>
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 
 <body>
 	<div class="padding-all">
 		<div class="header">
-			<h1><img src="./images/5.png" alt=" "> Gaming Login Form</h1>
+			<h1><img src="5.png" alt=" "> Gaming Login Form</h1>
 		</div>
 
 		<div class="design-w3l">
